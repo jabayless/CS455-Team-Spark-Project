34170960b73b100a22e3b59b062169df770f676e
imunro19 <ianmunro501@gmail.com>
2021-04-27 09:22:30 -0600

    added imports

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,7 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
-
-import java.util.Arrays;
-import java.util.List;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class Test {
 
@@ -15,22 +11,36 @@ public class Test {
             System.err.println("Usage: JavaWordCount <file>");
             System.exit(1);
         }
-        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        JavaRDD<String> lines = ctx.textFile(args[0], 1);
 
-        JavaRDD<String> words
-                = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
-        JavaPairRDD<String, Integer> ones
-                = words.mapToPair(word -> new Tuple2<>(word, 1));
-        JavaPairRDD<String, Integer> counts
-                = ones.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
+        SparkSession spark = SparkSession.builder().appName("Java Spark Example").getOrCreate();
+        DataFrameReader dataFrameReader = spark.read();
+        Dataset<Row> dataFrame = dataFrameReader.option("header", "true")
+                                                .option("ignoreLeadingWhiteSpace", "true")
+                                                .option("ignoreTrailingWhiteSpace", "true")
+                                                .option("mode", "DROPMALFORMED")
+                                                .csv("hdfs://juneau:49666/data/1996.csv");
 
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?, ?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
-        }
-        ctx.stop();
+        // path is found in core-site.xml
+        dataFrame.sample(1/1000.0).write().csv("hdfs://juneau:49666/home/spark/1996.csv");
+        spark.close();
+
+
+//        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+//        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+//        JavaRDD<String> lines = ctx.textFile(args[0], 1);
+//
+//        JavaRDD<String> words
+//                = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
+//        JavaPairRDD<String, Integer> ones
+//                = words.mapToPair(word -> new Tuple2<>(word, 1));
+//        JavaPairRDD<String, Integer> counts
+//                = ones.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
+//
+//        List<Tuple2<String, Integer>> output = counts.collect();
+//        for (Tuple2<?, ?> tuple : output) {
+//            System.out.println(tuple._1() + ": " + tuple._2());
+//        }
+//        ctx.stop();
     }
 
 }
