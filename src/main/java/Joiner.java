63442d3fde160ca9c58e82adaf697f0732ca1614
imunro19 <ianmunro501@gmail.com>
2021-04-28 00:17:21 -0600

    Finished this thing

--- src/main/java/Joiner.java
+++ src/main/java/Joiner.java
@@ -51,8 +51,8 @@ public class Joiner {
                 stations.col("STATION_ID").equalTo(data.col("station"))
         ).drop("station", "ELEVATION", "LNG", "LAT");
 
-        dataCities.printSchema();
-        dataCities.show();
+//        dataCities.printSchema();
+//        dataCities.show();
 //        truncCities.printSchema();
 //        truncCities.show();
 
@@ -61,37 +61,38 @@ public class Joiner {
                         .and(dataCities.col("STATE_ID").equalTo(truncCities.col("state_abbrv")))
         ).drop("state_abbrv");
 
-        dataCitiesStates.printSchema();
-        dataCitiesStates.show(500);
-        popDensity.printSchema();
-        popDensity.show();
+//        dataCitiesStates.printSchema();
+//        dataCitiesStates.show(500);
+//        popDensity.printSchema();
+//        popDensity.show();
 
         Dataset<Row> dataCitiesPop = dataCitiesStates.join(popDensity,
                 lower(dataCitiesStates.col("city")).equalTo(lower(popDensity.col("city_name")))
                         .and(lower(dataCitiesStates.col("state_name")).equalTo(lower(popDensity.col("State"))))
         ).drop("Index", "State", "2016 Population", "Land Area (Square Miles)", "city_name");
 
-        dataCitiesPop.printSchema();
-        dataCitiesPop.show(550);
-        colIndex.printSchema();
-        colIndex.show();
+//        dataCitiesPop.printSchema();
+//        dataCitiesPop.show(550);
+//        colIndex.printSchema();
+//        colIndex.show();
 
         Dataset<Row> dataCitiesPopCol = dataCitiesPop.join(colIndex,
                 dataCitiesPop.col("city").equalTo(colIndex.col("city_name"))
                         .and(dataCitiesPop.col("state_id").equalTo(colIndex.col("State")))
         ).drop("State", "city_name");
 
-        dataCitiesPopCol.printSchema();
-        dataCitiesPopCol.show();
-        hhIncome.printSchema();
-        hhIncome.show();
+//        dataCitiesPopCol.printSchema();
+//        dataCitiesPopCol.show();
+//        hhIncome.printSchema();
+//        hhIncome.show();
 
         Dataset<Row> completeJoin = dataCitiesPopCol.join(hhIncome,
                 dataCitiesPopCol.col("state_name").equalTo(hhIncome.col("State"))
         ).drop("State").withColumnRenamed("Median", "Median House Income");
 
-        completeJoin.printSchema();
-        completeJoin.show(500);
+//        completeJoin.printSchema();
+//        completeJoin.show(500);
+        completeJoin.coalesce(1).write().option("header", true).csv("hdfs://juneau:49666/spark/out");
 
         spark.close();
     }
