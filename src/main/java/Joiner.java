9ab5e93b5f67349cefc3e86541cdf559575209ad
imunro19 <ianmunro501@gmail.com>
2021-04-27 18:47:43 -0600

    reverted Test

--- src/main/java/Joiner.java
+++ src/main/java/Joiner.java
@@ -25,20 +25,24 @@ public class Joiner {
                 .option("ignoreTrailingWhiteSpace", true)
                 .csv("hdfs://juneau:49666/spark/data/uscities.csv");
 
+        // Drop extra data
         Dataset<Row> truncCities = cities.drop("city_ascii", "county_fips", "county_name", "lat", "lng", "population", "density", "source", "military", "incorporated", "timezone", "ranking", "zips", "id");
+        // Extract state from station id
         Dataset<Row> stateStations = stations.withColumn("STATE_ID", expr("substr(STATION_ID, 4, 2)"));
+        // Grab only 1 station from each city
         Dataset<Row> uniqueStations = stateStations.dropDuplicates("STATION_NAME");
+        // join based on city and state, drop extra, and sort
         Dataset<Row> joined = uniqueStations.join(truncCities,
                 lower(uniqueStations.col("STATION_NAME")).equalTo(lower(truncCities.col("city")))
                         .and(uniqueStations.col("STATE_ID").equalTo(truncCities.col("state_id")))
-        );
+        ).drop("STATION_NAME", "STATE_ID").sort("state_name");
 
-        states.printSchema();
-        stations.printSchema();
-        cities.printSchema();
-        truncCities.printSchema();
+//        states.printSchema();
+//        stations.printSchema();
+//        cities.printSchema();
+//        truncCities.printSchema();
 
-        joined.show();
+        joined.show(500);
 
         spark.close();
     }
