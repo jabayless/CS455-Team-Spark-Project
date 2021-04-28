dce97e747368c59484a6257983e671ea02970804
imunro19 <ianmunro501@gmail.com>
2021-04-27 18:23:27 -0600

    combined supp datasets

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,23 +3,46 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import static org.apache.spark.sql.functions.*;
+
 public class Test {
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length < 1) {
-            System.err.println("Usage: JavaWordCount <file>");
-            System.exit(1);
-        }
-
         SparkSession spark = SparkSession.builder().appName("Java Spark Example").getOrCreate();
-        Dataset<Row> dataFrame = spark.read()
+        Dataset<Row> states = spark.read()
                 .option("header", "true")
-                .csv("hdfs://juneau:49666/home/spark/data/2018.csv");
+                .option("ignoreLeadingWhiteSpace", true)
+                .option("ignoreTrailingWhiteSpace", true)
+                .csv("hdfs://juneau:49666/spark/data/states.csv");
+        Dataset<Row> stations = spark.read()
+                .option("header", "true")
+                .option("ignoreLeadingWhiteSpace", true)
+                .option("ignoreTrailingWhiteSpace", true)
+                .csv("hdfs://juneau:49666/spark/data/stations_condensed.csv");
+        Dataset<Row> cities = spark.read()
+                .option("header", "true")
+                .option("ignoreLeadingWhiteSpace", true)
+                .option("ignoreTrailingWhiteSpace", true)
+                .csv("hdfs://juneau:49666/spark/data/uscities.csv");
 
-        // path is found in core-site.xml
-//        dataFrame.sample(1/1000.0).write().csv("hdfs://juneau:49666/home/spark/1996.csv");
-        dataFrame.printSchema();
+        Dataset<Row> truncCities = cities.drop("city_ascii", "county_fips", "county_name", "lat", "lng", "population", "density", "source", "military", "incorporated", "timezone", "ranking", "zips", "id");
+        Dataset<Row> stateStations = stations.withColumn("STATE_ID", expr("substr(STATION_ID, 4, 2)"));
+        Dataset<Row> uniqueStations = stateStations.dropDuplicates("STATION_NAME");
+        Dataset<Row> joined = uniqueStations.join(truncCities,
+                lower(uniqueStations.col("STATION_NAME")).equalTo(lower(truncCities.col("city")))
+                        .and(uniqueStations.col("STATE_ID").equalTo(truncCities.col("state_id")))
+        );
+
+        states.printSchema();
+        stations.printSchema();
+        cities.printSchema();
+        truncCities.printSchema();
+
+        joined.show();
+//        uniqueStations.show();
+//        truncCities.show();
+
         spark.close();
 
 
