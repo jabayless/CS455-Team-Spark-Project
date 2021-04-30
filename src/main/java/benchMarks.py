b041b966d2e18ba2d8f31a28e457ae75442e717d
Jordan B <49706037+jabayless@users.noreply.github.com>
2021-04-30 13:55:09 -0600

    Update benchMarks.py

--- src/main/java/benchMarks.py
+++ src/main/java/benchMarks.py
@@ -10,6 +10,7 @@ import pandas as pd
 #script to get the runtime and memory uage of java program
 #   first argument to program is the path to the program
 #   second argument is the java program name
+#   example : clear & python3 benchMarks.py /home/jbayless/GitHub/Java/benchMarkScripts/javaTest/build/classes/java/main Test
 
 class BenchMark:
     def __init__(self, classPath, programName):
@@ -18,15 +19,17 @@ class BenchMark:
         self.startTime = time.time()
         self.memoryUsage = ""
         self.pid = 0
+        self.process = ""
 
     def getPid(self):
         #get the memory usage of the program
-        self.pid = subprocess.Popen(["java" , "-cp", self.classPath, self.programName], shell=False, stdout=subprocess.PIPE).pid
-        print (self.pid)
+        self.process = subprocess.Popen(["java" , "-cp", self.classPath, self.programName], shell=False, stdout=subprocess.PIPE)
+        self.pid = self.process.pid
         return self.pid
 
     def getMemoryUsage(self):
         memoryCommand = "sudo pmap " + str(self.getPid()) + " | tail -n 1"
+        time.sleep(1)
         javaProgram = subprocess.Popen(memoryCommand, shell=True, stdout=subprocess.PIPE)
         self.memoryUsage = str(javaProgram.stdout.read()).split(" ")[11][:-3]
         return self.memoryUsage
@@ -48,9 +51,10 @@ class BenchMark:
 
 def main():
         benchMark = BenchMark(sys.argv[1], sys.argv[2])
-        print ("Total memory usage is " + str(benchMark.getMemoryUsage()) + "." + 
-        " Time to complete is: " + str(benchMark.getRunTime()) + "s.")
-        benchMark.plotData()
+        print ("Total memory usage is " + str(benchMark.getMemoryUsage()) + ".")
+        benchMark.process.wait()
+        print ("Time to complete is " + str(time.time() - benchMark.startTime) + "s.")
+        #benchMark.plotData()
 
 if __name__ == '__main__':
     main()
